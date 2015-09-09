757a215753c2ffe92e07aab787d199addac0e4fa
Keith Yip <keithyip.kw@gmail.com>
2015-09-09 23:36:18 +0800

    Initial Commit

---
+++ FrameworkApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FrameworkApp
+//
+//  Created by Keith Yip on 9/9/15.
+//  Copyright (c) 2015 framework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FrameworkApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FrameworkApp
+//
+//  Created by Keith Yip on 9/9/15.
+//  Copyright (c) 2015 framework. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
